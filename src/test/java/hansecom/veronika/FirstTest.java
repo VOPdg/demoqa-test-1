94c7129e3350d2d4aabd033953852d65c67aab8e
veronikapodgol <veronika.podgol@gmail.com>
2021-12-16 18:06:28 +0300

    add gradle

---
+++ src/test/java/hansecom/veronika/FirstTest.java
@@ -0,0 +1,15 @@
+package hansecom.veronika;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+@DisplayName("Это наш первый тест")
+public class FirstTest {
+    @Test
+    void myFirstTest(){
+        System.out.println("Hello my Test");
+    }
+    public static void main(String[] args){
+        System.out.println("Hello you");
+    }
+
+}
